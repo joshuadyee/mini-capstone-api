860f64d8f83b4290984e223e10d16b742487d869
Joshua Yee <joshyee16@gmail.com>
2023-12-05 09:25:19 -0800

    delete non-RESTful code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,4 @@
 class ProductsController < ApplicationController
 
-  def index 
-    @products = Product.all
-    render template: "products/index"
-  end
 
-  def show
-    render json: {hi: "hello"}
-    # render template: "products/show"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get "/products", controller: "products", action: "index"
-  get "/first_product" => "products#show"
+  
 end
