3fd9281474988eae0ac21a0c6a6745f3e8c5cb7d
Joshua Yee <joshyee16@gmail.com>
2023-12-11 10:35:39 -0800

    update controller tests with happy/sad paths, update fixtures (description)

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -22,6 +22,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "create" do
     assert_difference "Product.count", 1 do
       post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description"  }
+      assert_response 200
+    end
+
+    assert_difference "Product.count", 0 do
+      post "/products.json", params: {}
+      assert_response 422
     end
   end
 
@@ -32,6 +38,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
+    assert_equal product.price.to_s, data["price"]
+    assert_equal product.image_url, data["image_url"]
+    assert_equal product.description, data["description"]
+
+    patch "/products/#{product.id}.json", params: { name: "" }
+    assert_response 422 
   end
 
   test "destroy" do
