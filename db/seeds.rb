ffb8541b9ad52282ca5fdcc791abf53d549545fa
Joshua Yee <joshyee16@gmail.com>
2023-12-11 11:53:48 -0800

    create supplier instances using seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-
-beanie = Product.new(
-  name: "beanie",
-  price: 30,
-  image_url: "https://newstaractive.com/cdn/shop/products/basic_beanie_reversible_49ers_chared_1.jpg?v=1507151403",
-  description: "gray 49ers beanie"
-)
-
-beanie.save
+supplier = Supplier.create(name: "Fanatics", email: "fanatics.com", phone_number: "111-121-7878")
