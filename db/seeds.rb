9f9b57d212a1642ee0366a208a984bd6e519c863
Joshua Yee <joshyee16@gmail.com>
2023-12-12 10:47:27 -0800

    add image instances in seeds, create has many/belongs to methods for product/image models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-supplier = Supplier.create(name: "Fanatics", email: "fanatics.com", phone_number: "111-121-7878")
+# supplier = Supplier.create(name: "Fanatics", email: "fanatics.com", phone_number: "111-121-7878")
+
+Image.create(url: "https://target.scene7.com/is/image/Target/GUEST_566a6895-f26b-4df8-a2fe-d82669a4ee7d?wid=488&hei=488&fmt=pjpeg", product_id: 2)
