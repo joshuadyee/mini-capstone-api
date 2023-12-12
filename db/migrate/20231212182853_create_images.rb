5d0b2599b9bb0d85c14b160b4d34c8853339c2e8
Joshua Yee <joshyee16@gmail.com>
2023-12-12 10:29:59 -0800

    rollback migration and destory Image model (typo), generate correct model and migrate

--- db/migrate/20231212182853_create_images.rb
+++ db/migrate/20231212182853_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[7.0]
   def change
     create_table :images do |t|
       t.string :url
-      t.integer :products_id
+      t.integer :product_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_12_182137) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_12_182853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "images", force: :cascade do |t|
     t.string "url"
-    t.integer "products_id"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
