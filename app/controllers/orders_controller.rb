f5e5e54f30fbe3bb7048caf60d5b1b30c79896f4
Joshua Yee <joshyee16@gmail.com>
2024-01-03 00:35:03 -0800

    add order jbuilder, index, show, and create actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,19 @@
 class OrdersController < ApplicationController
+  def index
+    @order = Order.all
+    render :index
+  end
+  
+  def show
+    @order = Order.find_by(id: params[:id])
+    # render template: "orders/show"
+    render :show
+  end
+  
   def create
+    p "current_user"
+    p current_user.id
+    p "current_user"
     @order = Order.new(
       user_id: params[:user_id],
       product_id: params[:product_id],
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   post "/sessions" => "sessions#create"
 
+  get "/orders" => "orders#index"
+  get "/orders/:id" => "orders#show"
   post "/orders" => "orders#create"
 end
